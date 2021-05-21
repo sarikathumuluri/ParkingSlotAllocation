34f05a4aed99737de6f4c8cb0d8a2018a35f9b0d
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB-Touch>
2021-05-21 09:51:07 +0530

    Modified login.java

--- ParkingSlotAllocation/src/com/capgemini/model/Login.java
+++ ParkingSlotAllocation/src/com/capgemini/model/Login.java
@@ -6,5 +6,37 @@ public class Login {
 	private User user;
 	private Role role;
 	
+	public String getLoginId() {
+		return loginId;
+	}
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	public Login(String loginId, String password, User user, Role role) {
+		super();
+		this.loginId = loginId;
+		this.password = password;
+		this.user = user;
+		this.role = role;
+	}
+	
 	// Constructors, Getter & Setter method
 }
